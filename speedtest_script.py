8bc3618525a7db7fc53de540aebfd7acc54a682d
Alex <alex.romer1@web.de>
2021-09-08 19:41:00 +0200

    Update speedtest_script.py

--- speedtest_script.py
+++ speedtest_script.py
@@ -8,27 +8,28 @@ try:
 except:
    print("please install speedtest-cli !!! --> pip3 install speedtest-cli")
 
+# Config
+data_path = '/home/pi/speedtest/'
 
-global output_file
-output_file = '/home/pi/Desktop/output_test.csv'
-
-
-def test():
+def st_test():
     st = speedtest.Speedtest()
     st.get_best_server()
     down = round(st.download() / 1e+6, 2)  #Mbit/s
     up = round(st.upload() / 1e+6, 2)  #Mbit/s
     return down, up
 
-def csv_write(row):
-    file = open(output_file, 'a+')
-    writer = csv.writer(file)
-    writer.writerow(row)
+def csv_write(data_row, output_file):
+    csv_file = open(output_file, 'a+')
+    writer = csv.writer(csv_file)
+    writer.writerow(data_row)
 
+def csv_read():
+    
 
 now = datetime.datetime.now()
 date = now.strftime("%Y-%m-%d")
 time = now.strftime("%H:%M:%S")
-row = [date, time] + list(test())
-csv_write(row)
+row = [date, time] + list(st_test())
+csv_file = data_path + now.strftime("%Y-%m") + '.csv'
+csv_write(row, csv_file)
 
